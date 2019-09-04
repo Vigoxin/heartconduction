523ddbd19c461e4b4f18a41f9969f352d590c447
Vigoxin <vigoxin@gmail.com>
2019-09-04 01:38:50 +0100

    changed colors of my tabs section

--- static/tabSystem.js
+++ static/tabSystem.js
@@ -1,15 +1,15 @@
-$('.tab').on('click', function() {
-  var idx =  $('.tab').index($(this));
+$('.my-tab').on('click', function() {
+  var idx =  $('.my-tab').index($(this));
   tabResponse(idx);
 })
 
 function tabResponse(idx) {
   $('.panel').css('backgroundColor', '').css('display', 'none');
-  $('.tab').css('backgroundColor', '')
+  $('.my-tab').css('backgroundColor', '');
   
   $('.panel').eq(idx).css('display', 'flex');
   $('.panel').eq(idx).css('transition', 'all 0.3s ease-out');
-  $('.tab').eq(idx).css('backgroundColor', $('.panel').eq(idx).css('backgroundColor'))
+  $('.my-tab').eq(idx).css('backgroundColor', $('.panel').eq(idx).css('backgroundColor'))
 }
 
 tabResponse(0);
