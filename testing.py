3839ecac0dd37dc02496868fd1432bcfa0f66bca
vig <vigdhildev@gmail.com>
2024-04-16 22:09:56 +0100

    removed some files not needed for digital ocean

--- testing.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# textures_path = 'static/textures/'
-# texture_files = [f for f in os.listdir(textures_path) if os.path.isfile(os.path.join(textures_path, f))]
-# print(texture_files)
-
-print(next(os.walk('./static/arrhythmias'))[1])
